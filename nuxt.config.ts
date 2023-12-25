464caf2cbc431f60d71377d899d31b693f3075ed
Tarabass <p.rietveld@live.com>
2023-12-25 12:49:28 +0100

    Add first blog with typography and component

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxt/content', '@nuxtjs/tailwindcss', 'nuxt-icon']
+  modules: ['@nuxt/content', '@nuxtjs/tailwindcss', 'nuxt-icon'],
+  content: {
+    highlight: {
+      theme: 'nord',
+      preload: ['ts', 'js', 'css', 'java', 'json', 'bash', 'vue']
+    }
+  }
 })
